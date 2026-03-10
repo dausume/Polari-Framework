3a9bb4a449bf1da3c57a04e073ea6268e83b75cf
Dustin Etts <dustinetts@gmail.com>
2026-03-10 00:40:43 -0400

    fixing display definitions with columns

--- polariApiServer/displayDefinition.py
+++ polariApiServer/displayDefinition.py
@@ -2,8 +2,12 @@ from objectTreeDecorators import treeObject, treeObjectInit
 
 class DisplayDefinition(treeObject):
     @treeObjectInit
-    def __init__(self, name='', description='', source_class='', definition='{}', manager=None):
+    def __init__(self, name='', description='', source_class='', definition='{}',
+                 linkedSolutions='[]', isPage=False, pageRoute='', manager=None):
         self.name = name
         self.description = description
         self.source_class = source_class  # The class this display belongs to
         self.definition = definition  # JSON blob of the full Display structure
+        self.linkedSolutions = linkedSolutions  # JSON array of solution names linked to this display
+        self.isPage = isPage  # Whether this display is published as a standalone page
+        self.pageRoute = pageRoute  # Custom route path when published as a page
