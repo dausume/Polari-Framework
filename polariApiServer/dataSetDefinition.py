26a78088a8c06ebf8831e6fdb9dd4538488a35c8
Dustin Etts <dustinetts@gmail.com>
2026-03-19 22:13:02 -0400

    Implementing field profiles and filter chains

--- polariApiServer/dataSetDefinition.py
+++ polariApiServer/dataSetDefinition.py
@@ -2,8 +2,10 @@ from objectTreeDecorators import treeObject, treeObjectInit
 
 class DataSetDefinition(treeObject):
     @treeObjectInit
-    def __init__(self, name='', description='', source_class='', definition='{}', manager=None):
+    def __init__(self, name='', description='', source_class='', definition='{}', field_profile_id='', filter_chain_id='', manager=None):
         self.name = name
         self.description = description
         self.source_class = source_class
         self.definition = definition
+        self.field_profile_id = field_profile_id
+        self.filter_chain_id = filter_chain_id
